ad4b9e5dd93f90145eaf8ce6abd6ec130dcc8ded
unknown <okal.ihor@gmail.com>
2017-05-13 00:59:09 +0300

    Setup & first 3 screens

---
+++ assets/README.md
@@ -0,0 +1,5 @@
+Physio Dashboard
+
+1) Home http://physio.div.agency<br />
+1) Home 2 http://physio.div.agency/home2.html<br />
+1) Sign Up http://physio.div.agency/sign-up.html<br />
---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+	// custom input[type="file"]
+
+	$("input[type=file]").change(function(){
+         var filename = $(this).val().replace(/.*\\/, "");
+         $("#filename").html(filename);
+    });
+});
