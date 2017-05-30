7b0bc56ff45f5675b34fbfc1f962d48810940fcf
unknown <okal.ihor@gmail.com>
2017-05-30 17:10:37 +0300

    Fixed issues #1 - #8

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,51 @@
 
 window.onload = function () {
 
+	//custom input[type="file"]
+
+	var input = document.getElementById("input-file"),
+			outPut = document.getElementById("filename");
+
+	if (input !== null) {
+		input.addEventListener("change", function () {
+			var filename = this.value.replace(/.*\\/, "");
+			outPut.innerHTML = filename;
+		});
+	}
+
+	//select exercise
+
+	var exerciseArr = document.querySelectorAll(".template-exercises ol li");
+	
+	if (exerciseArr.length > 0) {
+		for (var i = 0, len = exerciseArr.length; i < len; i++) {
+			exerciseArr[i].addEventListener('click', function () {
+				this.classList.toggle('active');
+			});
+		};
+	};
+
+
+	//edit program
+
+	// var editBtn = document.querySelector(".edit-name");
+
+	// if (editBtn !== null) {
+	// 	editBtn.addEventListener('click', function(e) {
+	// 		e.preventDefault();
+	// 		var target = document.querySelector(".program-setting");
+	// 		target.classList.toggle('active');
+	// 	});
+	// };
+
+
 	//delete program item
 	var deleteBtn = document.querySelectorAll(".delete");
 
 	for (var i = 0; i < deleteBtn.length; i++) {
 		deleteBtn[i].addEventListener("click", function(e) {
 			e.preventDefault();
-			this.closest("li").remove();
+			this.parentElement.parentElement.remove();
 		});
 	};
 
@@ -23,35 +61,67 @@ window.onload = function () {
 		connect: [false, true],
 		range: {
 			'min': [  1 ],
-			'max': [ 7 ]
+			'max': [ 10 ]
 		}
+	});
+
+	var outputRange1 = document.getElementById('range1-output');
+
+	range1.noUiSlider.on('update', function( values, handle ) {
+		var value = values[handle];
+		outputRange1.value = Math.round(value);
+
+		//you can write additional function here, which will work after slider changed
+
+
 	});
 
 	noUiSlider.create(range2, {
-		start: [ 18 ],
-		connect: [false, true],
-		range: {
-			'min': [  2 ],
-			'max': [ 30 ]
-		}
-	});
-
-	noUiSlider.create(range3, {
-		start: [ 3 ],
-		connect: [false, true],
-		range: {
-			'min': [  1 ],
-			'max': [ 7 ]
-		}
-	});
-
-	noUiSlider.create(range4, {
 		start: [ 5 ],
 		connect: [false, true],
 		range: {
 			'min': [  1 ],
-			'max': [ 7 ]
+			'max': [ 10 ]
 		}
+	});
+
+	var outputRange2 = document.getElementById('range2-output');
+
+	range2.noUiSlider.on('update', function( values, handle ) {
+		var value = values[handle];
+		outputRange2.value = Math.round(value);	
+	});
+
+	noUiSlider.create(range3, {
+		start: [ 1 ],
+		connect: [false, true],
+		range: {
+			'min': [  1 ],
+			'max': [ 5 ]
+		}
+	});
+
+	var outputRange3 = document.getElementById('range3-output');
+
+	range3.noUiSlider.on('update', function( values, handle ) {
+		var value = values[handle];
+		outputRange3.value = Math.round(value);	
+	});
+
+	noUiSlider.create(range4, {
+		start: [ 3 ],
+		connect: [false, true],
+		range: {
+			'min': [  1 ],
+			'max': [ 10 ]
+		}
+	});
+
+	var outputRange4 = document.getElementById('range4-output');
+
+	range4.noUiSlider.on('update', function( values, handle ) {
+		var value = values[handle];
+		outputRange4.value = Math.round(value);	
 	});
 
 	//costom select
@@ -60,13 +130,8 @@ window.onload = function () {
 	//tabs
 	$("#tabs").tabs();
 
-	//custom input[type="file"]
-
-	var input = document.getElementById("input-file"),
-			outPut = document.getElementById("filename");
-
-	input.addEventListener("change", function () {
-		var filename = this.value.replace(/.*\\/, "");
-		outPut.innerHTML = filename;
+	//sortable exercises
+	$(".template-exercises ol").sortable({
+		handle: ".move"
 	});
 };
